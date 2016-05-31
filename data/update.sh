aed6ce8b0b0c10b5906e15518698a76e855d8955
xavier <xavier@tttp.eu>
2016-05-31 13:31:33 +0200

    merge all 3 sources

--- data/update.sh
+++ data/update.sh
@@ -2,3 +2,13 @@
 
 echo "act_id,issue_date,voting_date" > date.csv
 jq -r ".[]|[.act_id,.issue_date,.voting_date]|@csv" print_doc_dates.log >> date.csv
+
+#add the votes in the EP
+#vi ep6.csv %s%(% (%g))
+#q "select v.*,ep.nb_amendment,ep.ep_date from votes.csv v left join ep6.csv ep on docnrinterinst=doc_id limit 1" -d, -H -O
+
+#the issue date (first date in the council)
+#q "select v.*,d.issue_date from votes.csv v left join date.csv as d on act=act_id" -d, -H -O 
+
+
+q "select v.*,ep.nb_amendment,ep.ep_date,d.issue_date from votes.csv v left join ep6.csv ep on docnrinterinst=doc_id left join date.csv as d on act=act_id" -d, -H -O > complete.csv
